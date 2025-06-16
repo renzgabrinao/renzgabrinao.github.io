0b9150f17f977456fb8dfe284ad2b53c8d9be69e
renzgabrinao <rgabrinao1@gmail.com>
2025-06-16 13:28:44 -0700

    Project Details start Change format of projects.json to have 'short_description' for project card and 'full_description' for project details page. Create ProjectDetails.css Add more elements in ProjectDetails.jsx to match mockup.

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -24,7 +24,7 @@ function Projects() {
                   key={project.id} 
                   id={project.id} 
                   title={project.title} 
-                  description={project.description}
+                  description={project.short_description}
                   img={project.img}
                 />
               ))
